0289ef87605361ceed1d3a16e0b1692bf4745b23
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-04-05 01:26:55 +0300

    Update JS file

--- vendor/autoprefixer.js
+++ vendor/autoprefixer.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-'use strict';
 
 ;(function(){
 
