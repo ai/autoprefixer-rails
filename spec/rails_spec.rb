de9ce410f1c22ced5d077b8fcab65da4ef2d28cf
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-04-09 13:27:35 +0300

    Fix Ruby 1.8 syntax

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -2,13 +2,13 @@ require File.expand_path('../spec_helper', __FILE__)
 
 describe CssController, :type => :controller do
   it "should integrate with Rails" do
-    get :test, file: 'test'
+    get :test, :file => 'test'
     response.should be_success
     response.body.should == PREFIXED
   end
 
   it "should not compile foreign styles" do
-    get :test, file: 'foreign'
+    get :test, :file => 'foreign'
     response.should be_success
     response.body.should == ".f { transition: none }\n"
   end
