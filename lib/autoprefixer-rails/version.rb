fde1a6cea0d496d895cf02debe2468aa39991fef
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-04-27 12:01:43 +0300

    Release minor version

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,3 +1,3 @@
 module AutoprefixerRails
-  VERSION = '0.3.20130424'.freeze unless defined? AutoprefixerRails::VERSION
+  VERSION = '0.3.20130427'.freeze unless defined? AutoprefixerRails::VERSION
 end
