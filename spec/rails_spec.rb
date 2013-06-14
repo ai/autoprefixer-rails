acb812177e670703a8c40ee4f759dc2878b9d3c6
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-06-14 19:21:19 +0700

    Remove old test and add cache cleaner

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,16 +1,14 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe CssController, :type => :controller do
+  before :all do
+    Rails.root.join('tmp').rmtree
+  end
+
   it "should integrate with Rails" do
     get :test, :file => 'test'
     response.should be_success
     response.body.should == PREFIXED
-  end
-
-  it "should not compile foreign styles" do
-    get :test, :file => 'foreign'
-    response.should be_success
-    response.body.should == ".f { transition: none }\n"
   end
 end
 
