e4dc2e6a84c853cf80d93dea1ed1c662d9f24f0c
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-06-25 23:24:08 +0800

    Use Rails 4 for tests

--- spec/app/config/environments/test.rb
+++ spec/app/config/environments/test.rb
@@ -2,7 +2,7 @@ App::Application.configure do
   config.cache_classes = true
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
-  config.whiny_nils = true
+  config.eager_load = false
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.action_dispatch.show_exceptions = false
