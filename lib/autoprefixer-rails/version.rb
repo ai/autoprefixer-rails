256f76c8009e2f6906f055cb8851e4e00c6f00d6
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-06-29 00:24:47 +0800

    Release 0.5.20130629

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,3 +1,3 @@
 module AutoprefixerRails
-  VERSION = '0.5.20130626'.freeze unless defined? AutoprefixerRails::VERSION
+  VERSION = '0.5.20130629'.freeze unless defined? AutoprefixerRails::VERSION
 end
