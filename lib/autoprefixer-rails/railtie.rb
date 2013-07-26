32d3e7323c2784b04d3913a059c2a01ee4c8eb9d
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-07-26 04:47:07 +0400

    Fix Rails initializer without Sprockets

--- lib/autoprefixer-rails/railtie.rb
+++ lib/autoprefixer-rails/railtie.rb
@@ -16,24 +16,27 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
-require 'sprockets/railtie'
+begin
+  require 'sprockets/railtie'
 
-module AutoprefixedRails
-  class Railtie < ::Rails::Railtie
-    rake_tasks do |app|
-      require 'rake/autoprefixer_tasks'
-      Rake::AutoprefixerTasks.new(browsers(app))
-    end
+  module AutoprefixedRails
+    class Railtie < ::Rails::Railtie
+      rake_tasks do |app|
+        require 'rake/autoprefixer_tasks'
+        Rake::AutoprefixerTasks.new(browsers(app))
+      end
 
-    initializer :setup_autoprefixer do |app|
-      AutoprefixerRails.install(app.assets, browsers(app))
-    end
+      initializer :setup_autoprefixer do |app|
+        AutoprefixerRails.install(app.assets, browsers(app))
+      end
 
-    # Read browsers requirements from application config
-    def browsers(app)
-      file   = app.root.join('config/autoprefixer.yml')
-      config = file.exist? ? YAML.load_file(file) : { 'browsers' => nil }
-      config['browsers']
+      # Read browsers requirements from application config
+      def browsers(app)
+        file   = app.root.join('config/autoprefixer.yml')
+        config = file.exist? ? YAML.load_file(file) : { 'browsers' => nil }
+        config['browsers']
+      end
     end
   end
+rescue LoadError
 end
