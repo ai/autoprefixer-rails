efe70ac1cb356517e673e8359f5e2be5366850d4
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-10-20 11:25:45 +0300

    Release 0.8.20131020 version

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,3 +1,3 @@
 module AutoprefixerRails
-  VERSION = '0.8.20131017'.freeze unless defined? AutoprefixerRails::VERSION
+  VERSION = '0.8.20131020'.freeze unless defined? AutoprefixerRails::VERSION
 end
