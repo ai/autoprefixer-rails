64c83ff3d7fdf1bd6e3b9d5b31fd2b69d067a758
Sasha Koss <koss@nocorp.me>
2013-11-05 09:45:55 +0000

    Update version to v0.8.20131104

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,3 +1,3 @@
 module AutoprefixerRails
-  VERSION = '0.8.20131029'.freeze unless defined? AutoprefixerRails::VERSION
+  VERSION = '0.8.20131104'.freeze unless defined? AutoprefixerRails::VERSION
 end
