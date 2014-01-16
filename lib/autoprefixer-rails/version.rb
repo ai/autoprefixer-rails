8dbf26c52c238af7794813d635f9c686d119c17d
Andrey Sitnik <andrey@sitnik.ru>
2014-01-17 02:52:30 +0400

    Release 1.0.20140117 version

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,3 +1,3 @@
 module AutoprefixerRails
-  VERSION = '1.0.20140110'.freeze unless defined? AutoprefixerRails::VERSION
+  VERSION = '1.0.20140117'.freeze unless defined? AutoprefixerRails::VERSION
 end
