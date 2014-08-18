6dcaf70b63a99bbbb751b53a85439532a0412edb
Andrey Sitnik <andrey@sitnik.ru>
2014-08-19 02:49:50 +0400

    Remove old hack for old Rails Sass

--- lib/autoprefixer-rails/sprockets.rb
+++ lib/autoprefixer-rails/sprockets.rb
@@ -16,43 +16,11 @@ module AutoprefixerRails
       @processor.process(css, from: input, to: output).css
     end
 
-    # Add prefixes only if in `content` will be CSS.
-    def process_only_css(context, content)
-      begin
-        process(context, content)
-      rescue ExecJS::ProgramError => e
-        if e.message =~ /Can't parse CSS/
-          content
-        else
-          raise e
-        end
-      end
-    end
-
     # Register postprocessor in Sprockets depend on issues with other gems
     def install(assets)
-      if ignore_syntax_error?
-        register(assets) { |context, css| process_only_css(context, css) }
-      else
-        register(assets) { |context, css| process(context, css) }
+      assets.register_postprocessor('text/css', :autoprefixer) do |context, css|
+        process(context, css)
       end
-    end
-
-    private
-
-    # Add `block` as `assets` postprocessor
-    def register(assets, &block)
-      assets.register_postprocessor('text/css', :autoprefixer, &block)
-    end
-
-    # Return true if broken sass-rails is loaded
-    def ignore_syntax_error?
-      return false unless defined? Sass::Rails
-
-      fixed   = Gem::Version.new('4.0.1')
-      current = Gem::Version.new(Sass::Rails::VERSION)
-
-      current < fixed
     end
   end
 end
