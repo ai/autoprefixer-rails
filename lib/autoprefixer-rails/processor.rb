edd5c4ba95b3a672601e2636db14ebff24d1f56f
Andrey Sitnik <andrey@sitnik.ru>
2015-01-20 18:42:17 +0300

    Fix work without Rails

--- lib/autoprefixer-rails/processor.rb
+++ lib/autoprefixer-rails/processor.rb
@@ -42,7 +42,7 @@ module AutoprefixerRails
     # Convert params to JS string and add browsers from Browserslist config
     def js_params(from = nil)
       unless from
-        if defined? Rails
+        if defined? Rails and Rails.root
           from = Rails.root.join('app/assets/stylesheets').to_s
         else
           from = '.'
