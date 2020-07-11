8f429d7ac4187cc440db9a507c851b145251bb4d
Andrey Sitnik <andrey@sitnik.ru>
2020-07-10 22:49:02 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p pkg/
-podman build . --tag ai/autoprefixer-rails
-podman run --privileged --rm -v ./pkg/:/var/app/pkg/ ai/autoprefixer-rails:latest
+sudo podman build . --tag ai/autoprefixer-rails
+sudo podman run --privileged --rm -v ./pkg/:/var/app/pkg/ ai/autoprefixer-rails:latest
