0b3939c2b59b50599f1deacccff7e9802b58f409
Albert Song <albb0920@gmail.com>
2020-11-05 11:07:51 +0800

    build/url.js: fix `replace()` not defined for URL
    Input.origin passes URL object rather than strings, our shim doesn't
    handle this case.
    
    Fixes #194

--- build/url.js
+++ build/url.js
@@ -3,6 +3,6 @@ module.exports = Object.assign(
   require("rollup-plugin-node-polyfills/polyfills/url.js"),
   {
     pathToFileURL: (path)=> { return `file:///${encodeURIComponent(path)}` },
-    fileURLToPath: (fileURL)=> { return decodeURIComponent(fileURL.replace(/^file:\/\/\//, '')) }
+    fileURLToPath: (fileURL)=> { return decodeURIComponent(fileURL.toString().replace(/^file:\/\/\//, '')) }
   }
 )
