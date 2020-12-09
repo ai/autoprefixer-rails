f2806828a6d50c25c8b0c830196752362e58a9d9
Albert Song <albb0920@gmail.com>
2020-12-09 18:54:19 +0800

    Release 10.1.0.0

--- lib/autoprefixer-rails/version.rb
+++ lib/autoprefixer-rails/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module AutoprefixerRails # :nodoc:
-  VERSION = "10.0.3.0" unless defined? AutoprefixerRails::VERSION
+  VERSION = "10.1.0.0" unless defined? AutoprefixerRails::VERSION
 end
